c2866c495b61a5deb99354d6fc43e9ebeb1cec68
Tyler <tylerwertman@outlook.com>
2023-02-18 17:54:53 -0500

    Hook Form Project

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,8 @@ const [firstName, setFirstName] = useState("");
 const [lastName, setLastName] = useState("");
 const [age, setAge] = useState("");
 const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [password2, setPassword2] = useState("");
 
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,7 +18,7 @@ const handleSubmit = (e) => {
         <div>
             <form action="" className="form col-md-4 mx-auto" onSubmit={(handleSubmit)}>
                 <h1>Please fill out this form!</h1>
-                <div className="form-group mt-3">
+                <div className="form-group mt-3 flex">
                     <label htmlFor="" className="form-label">First Name: </label>
                     <input type="text" className="form-control" onChange={(e)=>setFirstName(e.target.value)}/>
                 </div>
@@ -24,15 +26,33 @@ const handleSubmit = (e) => {
                     <label htmlFor="" className="form-label">Last Name: </label>
                     <input type="text" className="form-control" onChange={(e)=>setLastName(e.target.value)} />
                 </div>
-                <div className="form-group mt-3">
+                {/* <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Age: </label>
                     <input type="number" name="" id="" className="form-control" onChange={(e)=>setAge(e.target.value)} />
-                </div>
+                </div> */}
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Email: </label>
                     <input type="email" name="" id="" className="form-control" onChange={(e)=>setEmail(e.target.value)} />
                 </div>
-                <button className="btn btn-success mt-3">Submit</button>
+                <div className="form-group mt-3">
+                    <label htmlFor="" className="form-label">Password: </label>
+                    <input type="password" name="" id="" className="form-control" onChange={(e)=>setPassword(e.target.value)} />
+                </div>
+                <div className="form-group mt-3">
+                    <label htmlFor="" className="form-label">Confirm Password: </label>
+                    <input type="password" name="" id="" className="form-control" onChange={(e)=>setPassword2(e.target.value)} />
+                </div>
+                <div>
+                    <p className="mt-3">Your form data:</p>
+                </div>
+                <div>
+                    <p>First Name: {firstName}</p>
+                    <p>Last Name: {lastName}</p>
+                    <p>Email: {email}</p>
+                    <p>Password: {password}</p>
+                    <p>Confirm Password: {password2}</p>
+                </div>
+                {/* <button className="btn btn-success mt-3">Submit</button> */}
             </form>
         </div>
     )
