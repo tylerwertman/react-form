6d41360e9ebdba77c143fe9f2c2e183e50b0358f
Tyler <tylerwertman@outlook.com>
2023-02-22 15:24:11 -0500

    add validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ const Form = () => {
 
 const [firstName, setFirstName] = useState("");
 const [lastName, setLastName] = useState("");
-// const [age, setAge] = useState("");
+const [age, setAge] = useState("");
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
 const [password2, setPassword2] = useState("");
@@ -21,10 +21,16 @@ const handleSubmit = (e) => {
                 <div className="form-group mt-3 flex">
                     <label htmlFor="" className="form-label">First Name: </label>
                     <input type="text" className="form-control" onChange={(e)=>setFirstName(e.target.value)}/>
+                    {
+                        firstName && firstName.length < 3 ? <p className='text-danger'>First Name must be longer than 2 characters</p> : ""
+                    }
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Last Name: </label>
                     <input type="text" className="form-control" onChange={(e)=>setLastName(e.target.value)} />
+                    {
+                        lastName && lastName.length < 3 ? <p className='text-danger'>Last Name must be longer than 2 characters</p> : ""
+                    }
                 </div>
                 {/* <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Age: </label>
@@ -33,14 +39,23 @@ const handleSubmit = (e) => {
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Email: </label>
                     <input type="email" name="" id="" className="form-control" onChange={(e)=>setEmail(e.target.value)} />
+                    {
+                        email && email.length < 3 ? <p className='text-danger'>Email must be longer than 2 characters</p> : ""
+                    }
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Password: </label>
                     <input type="password" name="" id="" className="form-control" onChange={(e)=>setPassword(e.target.value)} />
+                    {
+                        password && password.length < 9 ? <p className='text-danger'>Password must be longer than 8 characters</p> : ""
+                    }
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Confirm Password: </label>
                     <input type="password" name="" id="" className="form-control" onChange={(e)=>setPassword2(e.target.value)} />
+                    {
+                        password2.length > 0 && password2 === password ? <p className="text-success">Passwords match</p> : <p className="text-danger">Passwords do not match</p>
+                    }
                 </div>
                 <div>
                     <p className="mt-3">Your form data:</p>
